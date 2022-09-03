a241e251a3c5e38ab26cb8ad3fa1c5b95f19d5cf
SeNicko <sebastianflajszer@gmail.com>
2022-09-03 21:42:24 +0200

    Init

---
+++ astro.config.mjs
@@ -0,0 +1,16 @@
+import { defineConfig } from "astro/config";
+import mdx from "@astrojs/mdx";
+
+import tailwind from "@astrojs/tailwind";
+
+// https://astro.build/config
+export default defineConfig({
+  integrations: [
+    mdx(),
+    tailwind({
+      config: {
+        applyBaseStyles: false,
+      },
+    }),
+  ],
+});
